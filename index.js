08d1a35d28b7202b9b50e42649388f48ed2ee9b3
Vignesh-2896 <vigneshb2896@gmail.com>
2021-06-13 16:54:09 +0530

    Local Storage and Firebase.

--- index.js
+++ index.js
@@ -3,12 +3,39 @@ let myLibrary = [];
 let editBookDiv = document.querySelector("#edit_book_details");
 let tbody = document.querySelector("#table_body");
 let formElements = document.getElementById("add_new_book_form").elements;
+let storageType = "";
+
+function Book(bookTitle, bookAuthor, bookLength, bookStatus = "To Be Read"){
+    this.name = bookTitle;
+    this.author = bookAuthor;
+    this.pages = bookLength;
+    this.status = bookStatus;
+};
+
+function BookArray(){
+
+}
+
+BookArray.prototype.fetchBook = function(){
+    tmpArray = [this.name, this.author, this.pages, this.status];
+    return tmpArray;
+};
+
+BookArray.prototype.updateStatus = function(editBookStatus){
+    this.status = editBookStatus;
+}
+
+Book.prototype = Object.create(BookArray.prototype);
 
 let addBookDisplay = document.querySelector("#add_book_button");
 addBookDisplay.addEventListener("click", function(){
-    let addBookDiv = document.querySelector("#add_new_book");
-    addBookDiv.style.visibility = "visible";
-    addBookDiv.style.height = "auto"; 
+    if(storageType != ""){
+        let addBookDiv = document.querySelector("#add_new_book");
+        addBookDiv.style.visibility = "visible";
+        addBookDiv.style.height = "auto";
+    } else {
+        alert("Please select a storage type before adding a new book.")
+    }
 });
 
 let addBookSubmit = document.querySelector("#add_book_submit");
@@ -44,32 +71,18 @@ editBookDiv.addEventListener("click",function(){
         editBookDiv.style.visibility = "hidden";
         editBookDiv.style.height = "0%";   
         displayLibrary();
+        updateStorage();     
     } else {
         alert("Same Book Status selected. Please choose a different one.")
     }
 });
 
-function Book(bookTitle, bookAuthor, bookLength, bookStatus = "To Be Read"){
-    this.name = bookTitle;
-    this.author = bookAuthor;
-    this.pages = bookLength;
-    this.status = bookStatus;
-};
-
-function BookArray(){
-
-}
-
-BookArray.prototype.fetchBook = function(){
-    tmpArray = [this.name, this.author, this.pages, this.status];
-    return tmpArray;
-};
-
-BookArray.prototype.updateStatus = function(editBookStatus){
-    this.status = editBookStatus;
-}
-
-Book.prototype = Object.create(BookArray.prototype);
+let storageDisplay = document.querySelector("#select_storage_button");
+storageDisplay.addEventListener("click", function(){
+    let storageDiv = document.querySelector("#select_storage");
+    storageDiv.style.visibility = "visible";
+    storageDiv.style.height = "auto"; 
+});
 
 function addBookToLibrary(){
     let bookTitle = formElements[0].value;
@@ -82,8 +95,8 @@ function addBookToLibrary(){
     B1 = new Book(bookTitle, bookAuthor, bookLength, bookStatus);
     
     myLibrary.push(B1);
-
     displayLibrary();
+    updateStorage();
 };
 
 function displayLibrary(){
@@ -92,7 +105,6 @@ function displayLibrary(){
     while(tbody.firstChild){
         tbody.removeChild(tbody.firstChild);
     }
-    
     myLibrary.forEach(function(currentBook){
         
         let tr = document.createElement("tr");
@@ -156,4 +168,60 @@ function deleteBook(curBookRow){
     bookObjectPostition = curBookRow.getAttribute("data-list-pos");
     delete myLibrary[bookObjectPostition];
     displayLibrary();
+    updateStorage(); 
 };
+
+if(localStorage.getItem("books-array")){
+
+}
+
+function setupStorage(storageSelected){
+    storageType = storageSelected.getAttribute("data-storage");
+    let storageDiv = document.querySelector("#select_storage");
+    storageDiv.style.visibility = "hidden";
+    storageDiv.style.height = "0%";     
+
+    if(storageType == "local" && localStorage.getItem("books-array")){
+        fetchedBookList = JSON.parse(localStorage.getItem("books-array"));
+        fetchedBookList.forEach(function(fetchedBook){
+            if(fetchedBook != null){
+                b = new Book();
+                Object.assign(b,fetchedBook);
+                myLibrary.push(b);
+            }
+        });
+    } else {
+          // Your web app's Firebase configuration
+        var firebaseConfig = {
+            apiKey: "AIzaSyC4mgJoiumM9JMgKBgf2-JVV1u4F7eBFUI",
+            authDomain: "library-app-ced96.firebaseapp.com",
+            projectId: "library-app-ced96",
+            databaseURL: "https://library-app-ced96-default-rtdb.firebaseio.com/",
+            storageBucket: "library-app-ced96.appspot.com",
+            messagingSenderId: "992271486475",
+            appId: "1:992271486475:web:af7a28e493e71130b7c10e"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+        var databaseStuff = firebase.database().ref("BookObjects/");
+        databaseStuff.on("value",function(snapshot){
+            snapshot.val().forEach(function(fetchedBook){
+                b = new Book();
+                Object.assign(b,fetchedBook)
+                myLibrary.push(b);
+            });          
+        });
+    }
+    setTimeout(function(){
+        displayLibrary();
+    },5000);
+}
+
+function updateStorage(){
+    if (storageType == "local"){
+        localStorage.clear();
+        localStorage.setItem("books-array",JSON.stringify(myLibrary));
+    } else {
+        firebase.database().ref("BookObjects/").set(myLibrary);
+    }
+}
