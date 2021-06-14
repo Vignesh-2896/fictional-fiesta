5cda87a279cd58a2914a1ce4af0ba5248baf145d
Vignesh-2896 <vigneshb2896@gmail.com>
2021-06-14 10:36:40 +0530

    Final Touches

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-let myLibrary = [];
+let myLibrary = []; // Array to hold object's of Book.
 
 let editBookDiv = document.querySelector("#edit_book_details");
 let tbody = document.querySelector("#table_body");
 let formElements = document.getElementById("add_new_book_form").elements;
 let storageType = "";
+let currentDisplay = "";
 
-function Book(bookTitle, bookAuthor, bookLength, bookStatus = "To Be Read"){
+function Book(bookTitle, bookAuthor, bookLength, bookStatus){
     this.name = bookTitle;
     this.author = bookAuthor;
     this.pages = bookLength;
@@ -16,33 +17,32 @@ function BookArray(){
 
 }
 
-BookArray.prototype.fetchBook = function(){
+BookArray.prototype.fetchBook = function(){ // Function to retrieve all properties of a Book Object as an Array
     tmpArray = [this.name, this.author, this.pages, this.status];
     return tmpArray;
 };
 
-BookArray.prototype.updateStatus = function(editBookStatus){
+BookArray.prototype.updateStatus = function(editBookStatus){    // Function to update status property for a Book Status
     this.status = editBookStatus;
 }
 
-Book.prototype = Object.create(BookArray.prototype);
+Book.prototype = Object.create(BookArray.prototype);        // Attach prototype functions.
 
 let addBookDisplay = document.querySelector("#add_book_button");
-addBookDisplay.addEventListener("click", function(){
-    if(storageType != ""){
+addBookDisplay.addEventListener("click", function(){    // Function to display form to input new books.
+    if(storageType != ""){          // Addition of new books is only possible after Storage type has been selected.
         let addBookDiv = document.querySelector("#add_new_book");
-        addBookDiv.style.visibility = "visible";
-        addBookDiv.style.height = "auto";
+        displayDiv(addBookDiv);
     } else {
         alert("Please select a storage type before adding a new book.")
     }
 });
 
 let addBookSubmit = document.querySelector("#add_book_submit");
-addBookSubmit.addEventListener("click",function(){
+addBookSubmit.addEventListener("click",function(){  // Function to add new books to list based on data input in form.
 
     proceedToAddFlag = true;
-    for(let i=0;i<formElements.length-1;i++){
+    for(let i=0;i<formElements.length-1;i++){       // Validation to check if all form elements for adding a book are present.
         if(formElements[i].value == ""){
             proceedToAddFlag = false;
         }
@@ -51,26 +51,22 @@ addBookSubmit.addEventListener("click",function(){
     if(proceedToAddFlag){
         addBookToLibrary();
         let addBookDiv = document.querySelector("#add_new_book");
-        addBookDiv.style.visibility = "hidden"; 
-        addBookDiv.style.height = "0%";         
+        hideDiv(addBookDiv);    
     } else {
         alert("Kindly enter value to all the fields.")
     }
 
 });
 
-let editBookSubmit = document.querySelector("edit_book_submit");
-editBookDiv.addEventListener("click",function(){
-
+editBookDiv.addEventListener("click",function(){    // Function to Edit status of Book
     let editBookSubmit = document.getElementById("edit_book_form").elements;
 
-    bookObjectPostition = document.getElementById("edit_book_status").getAttribute("data-list-pos");
+    bookObjectPostition = document.getElementById("edit_book_status").getAttribute("data-list-pos"); // Get Array position of the Object in myLibrary array
     requiredBookObject = myLibrary[bookObjectPostition];
     if(requiredBookObject.status != editBookSubmit[0].value){
         requiredBookObject.updateStatus(document.getElementById("edit_book_status").value)
-        editBookDiv.style.visibility = "hidden";
-        editBookDiv.style.height = "0%";   
-        displayLibrary();
+        hideDiv(editBookDiv);
+        displayLibrary();       // Once a Book's status has been updated, the table is rebuilt once more.
         updateStorage();     
     } else {
         alert("Same Book Status selected. Please choose a different one.")
@@ -78,10 +74,9 @@ editBookDiv.addEventListener("click",function(){
 });
 
 let storageDisplay = document.querySelector("#select_storage_button");
-storageDisplay.addEventListener("click", function(){
+storageDisplay.addEventListener("click", function(){        // Function to display storage choices.
     let storageDiv = document.querySelector("#select_storage");
-    storageDiv.style.visibility = "visible";
-    storageDiv.style.height = "auto"; 
+    displayDiv(storageDiv);
 });
 
 function addBookToLibrary(){
@@ -92,7 +87,7 @@ function addBookToLibrary(){
 
     document.getElementById("add_new_book_form").reset();
 
-    B1 = new Book(bookTitle, bookAuthor, bookLength, bookStatus);
+    B1 = new Book(bookTitle, bookAuthor, bookLength, bookStatus);   // New object created with the inputs from the form.
     
     myLibrary.push(B1);
     displayLibrary();
@@ -102,7 +97,7 @@ function addBookToLibrary(){
 function displayLibrary(){
     let totalBookCount = 1;
 
-    while(tbody.firstChild){
+    while(tbody.firstChild){            // Before displaying elements, current contents of table are deleted.
         tbody.removeChild(tbody.firstChild);
     }
     myLibrary.forEach(function(currentBook){
@@ -114,18 +109,18 @@ function displayLibrary(){
         tr.appendChild(td0);
 
         let bookDets = currentBook.fetchBook();
-        for(let i=0;i<bookDets.length;i++){
+        for(let i=0;i<bookDets.length;i++){         // Loop to display all properties of current book.
             let td1 = document.createElement("td");
             td1.textContent = bookDets[i];
             tr.appendChild(td1);
         }
 
         addButtons = ["edit","delete"];
-        addButtons.forEach(function(curButton){
+        addButtons.forEach(function(curButton){     // Loop to add Edit and Delete buttons for current Book.
             let td5 = document.createElement("td");
             let button = document.createElement("button");
-            button.setAttribute("data-list-pos",totalBookCount - 1)
-            button.setAttribute("id",curButton);
+            button.setAttribute("data-list-pos",totalBookCount - 1)     // To identify, which book object's details are present in any row, 
+            button.setAttribute("id",curButton);                        // data attribute is used to show the array position of the object in myLibrary array.
             button.textContent = curButton.charAt(0).toUpperCase() + curButton.slice(1);
             td5.appendChild(button);
             tr.appendChild(td5);
@@ -135,7 +130,7 @@ function displayLibrary(){
 
         tbody.appendChild(tr);
     });
-
+// Attaching functions to Edit and Delete buttons.
     document.querySelectorAll("#edit").forEach(editButton => {
         editButton.addEventListener("click",function(){
             displayEdit(editButton);
@@ -150,13 +145,12 @@ function displayLibrary(){
 
 };
 
-function displayEdit(curBookRow){
+function displayEdit(curBookRow){ // function to edit a book.
 
     bookObjectPostition = curBookRow.getAttribute("data-list-pos");
     requiredBookObject = myLibrary[bookObjectPostition];
 
-    editBookDiv.style.visibility = "visible";
-    editBookDiv.style.height = "auto"; 
+    displayDiv(editBookDiv); 
 
     document.getElementById("edit_book_id").textContent = requiredBookObject.name;
     document.getElementById("edit_book_status").value = requiredBookObject.status;
@@ -164,32 +158,21 @@ function displayEdit(curBookRow){
 
 };
 
-function deleteBook(curBookRow){
+function deleteBook(curBookRow){        // function to delete a book.
     bookObjectPostition = curBookRow.getAttribute("data-list-pos");
     delete myLibrary[bookObjectPostition];
     displayLibrary();
     updateStorage(); 
 };
 
-if(localStorage.getItem("books-array")){
-
-}
-
-function setupStorage(storageSelected){
+function setupStorage(storageSelected){     // function to setup Storage connections.
     storageType = storageSelected.getAttribute("data-storage");
     let storageDiv = document.querySelector("#select_storage");
-    storageDiv.style.visibility = "hidden";
-    storageDiv.style.height = "0%";     
+    hideDiv(storageDiv);
 
     if(storageType == "local" && localStorage.getItem("books-array")){
         fetchedBookList = JSON.parse(localStorage.getItem("books-array"));
-        fetchedBookList.forEach(function(fetchedBook){
-            if(fetchedBook != null){
-                b = new Book();
-                Object.assign(b,fetchedBook);
-                myLibrary.push(b);
-            }
-        });
+        fetchedBookList.forEach(pushToLibrary);    // Data is fetched from local Storage and pushed to the myLibrary array.
     } else {
           // Your web app's Firebase configuration
         var firebaseConfig = {
@@ -205,23 +188,45 @@ function setupStorage(storageSelected){
         firebase.initializeApp(firebaseConfig);
         var databaseStuff = firebase.database().ref("BookObjects/");
         databaseStuff.on("value",function(snapshot){
-            snapshot.val().forEach(function(fetchedBook){
-                b = new Book();
-                Object.assign(b,fetchedBook)
-                myLibrary.push(b);
-            });          
+            snapshot.val().forEach(pushToLibrary);  // Data is fetched from Firebase DB and pushed to the myLibrary array.        
         });
     }
+    alert("Please give a few seconds to setup connection to your Storage choice.")    
     setTimeout(function(){
         displayLibrary();
-    },5000);
+        alert("You can start adding books now !");
+    },4000);
 }
 
-function updateStorage(){
+function updateStorage(){       // Function to update the storage. The contents of myLibrary array are stored since they hold all the required objects.
     if (storageType == "local"){
         localStorage.clear();
         localStorage.setItem("books-array",JSON.stringify(myLibrary));
     } else {
         firebase.database().ref("BookObjects/").set(myLibrary);
+    }
+}
+
+function hideDiv(divElement){   // Function to hide a Div Element after its processing is complete.
+    divElement.style.visibility = "hidden"; 
+    divElement.style.height = "0%"; 
+    currentDisplay = "";
+}
+
+function displayDiv(divElement){    // Function to Display a Div Element for user input.
+    if(currentDisplay == ""){
+        divElement.style.visibility = "visible";
+        divElement.style.height = "auto";
+        currentDisplay = divElement.getAttribute("name");
+    } else {        // to avoid overlap of multiple DIVs, at a time only one DIV Element can be displayed.
+        alert("Your request operation can't be processed. Currenly "+currentDisplay+" operation is being serviced.");
+    }
+}
+
+function pushToLibrary(fetchedBook){    // Function to append contents of storage to myLibrary array.
+    if(fetchedBook != null){
+        b = new Book();
+        Object.assign(b,fetchedBook)    // Assign function is used to copy the stored content details into a new Book object.
+        myLibrary.push(b);              // This will help in further processing.
     }
 }
